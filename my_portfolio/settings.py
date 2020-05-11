c61212d1496d90d9ddeb61ef926b633f371e25b8
Surya <suryateddy9999@gmail.com>
2020-05-11 03:39:14 +0000

    Remove old files

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!r$o4-qx*9!*_lbxdwvcnb0#u4zt66rx8c1(2r656sjxq_=ur+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['evilboruto7.pythonanywhere.com']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL =  '/media/'
 
